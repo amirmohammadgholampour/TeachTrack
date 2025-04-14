22085009df47267a35ca9237d9e9c88c0fe46823
Your Name <you@example.com>
2025-04-14 19:55:53 +0330

    Create field model

--- field/models.py
+++ field/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+class Field(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Name"
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True 
+    )
+
+    def __str__(self):
+        return self.name
