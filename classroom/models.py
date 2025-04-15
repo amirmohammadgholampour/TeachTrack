7d40801317c304e15246df73ee4613c21d40e751
Your Name <you@example.com>
2025-04-15 21:17:45 +0330

    Create class room model

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from django.db import models 
+from field.models import Field 
+from config import settings
 
-# Create your models here.
+class ClassRoom(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name="ClassRoom Name" 
+    )
+
+    base = models.PositiveIntegerField(
+        verbose_name="Base of student"
+    )
+
+    field = models.ForeignKey(
+        Field,
+        on_delete=models.CASCADE,
+        verbose_name="Field"
+    )
+
+    students = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="classroom",
+        on_delete=models.CASCADE,
+        limit_choices_to={"user_type": "student"}
+    )
+
+    teachers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="teaching_classroom",
+        limit_choices_to={"user_type": "teacher"}
+    )
+
+    def __str__(self):
+        return f'{self.name}(base: {self.base})(field: {self.field__name})'
