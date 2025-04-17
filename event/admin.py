db766f7b47ce64509eb61c10f90e8b7b8a1bf951
Your Name <you@example.com>
2025-04-17 08:32:02 +0330

    Create admin panel of event model

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib import messages
+from event.models import Event
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'description',
+        'image',
+        'created_at'
+    ]
+    search_fields = ['name', 'description']
+    ordering = ['name']
+    list_per_page = 10
+    list_filter = ['created_at']
+
+    # Custom action to deactivate selected events
+    @admin.action(description='Deactivate selected events')
+    def deactivate_events(self, request, queryset):
+        updated_count = queryset.update(is_active=False)
+        self.message_user(
+            request,
+            f'{updated_count} event(s) were successfully deactivated.',
+            messages.SUCCESS
+        )
+    actions = [deactivate_events]
