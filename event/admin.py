60286230f6b5ca9b61b767b8a3b20166a1bf4c65
Your Name <you@example.com>
2025-04-17 09:23:16 +0330

    Remove action from event admin

--- event/admin.py
+++ event/admin.py
@@ -14,14 +14,3 @@ class EventAdmin(admin.ModelAdmin):
     ordering = ['name']
     list_per_page = 10
     list_filter = ['created_at']
-
-    # Custom action to deactivate selected events
-    @admin.action(description='Deactivate selected events')
-    def deactivate_events(self, request, queryset):
-        updated_count = queryset.update(is_active=False)
-        self.message_user(
-            request,
-            f'{updated_count} event(s) were successfully deactivated.',
-            messages.SUCCESS
-        )
-    actions = [deactivate_events]
