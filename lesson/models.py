dc6988ef570897d4e96ab13a933c6bce437af86c
Your Name <you@example.com>
2025-04-17 06:17:55 +0330

    Create lesson model

--- lesson/models.py
+++ lesson/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db import models 
+from config import settings 
 
-# Create your models here.
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Name of lesson"
+    )
+
+    teachers = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE, 
+        limit_choices_to={"user_type":"teacher"},
+        related_name="teacher_lesson" 
+    )
+
+    def __str__(self):
+        return f"{self.name}({self.teachers})"
