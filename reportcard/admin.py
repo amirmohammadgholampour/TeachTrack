03199d8af819f8d18603f5a196decc45320f1427
Your Name <you@example.com>
2025-04-17 09:35:16 +0330

    Create admin panel of reportcard model

--- reportcard/admin.py
+++ reportcard/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from reportcard.models import ReportCard
+from django.contrib import messages
 
-# Register your models here.
+@admin.register(ReportCard)
+class ReportCardAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'class_room',
+        'disciplinary_status',
+        'grade',
+        'created_at'
+    ]
+    search_fields = ['user__username']
+    ordering = ['grade', 'class_room']
+    list_per_page = 10
+    list_filter = ['class_room']
