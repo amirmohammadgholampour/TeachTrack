06567b4e850024191a278e6567a4fac853a54a28
Your Name <you@example.com>
2025-04-17 09:23:31 +0330

    Remove action from ield admin

--- field/admin.py
+++ field/admin.py
@@ -12,14 +12,3 @@ class FieldAdmin(admin.ModelAdmin):
     ordering = ['name']
     list_per_page = 10
     list_filter = ['created_at']
-
-    # Custom action to deactivate selected fields
-    @admin.action(description='Deactivate selected fields')
-    def deactivate_fields(self, request, queryset):
-        updated_count = queryset.update(is_active=False)
-        self.message_user(
-            request,
-            f'{updated_count} field(s) were successfully deactivated.',
-            messages.SUCCESS
-        )
-    actions = [deactivate_fields]
