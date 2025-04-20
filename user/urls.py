41e2cbe8011d9966b9706308977ce8b63120f283
Your Name <you@example.com>
2025-04-20 17:57:22 +0330

    Add UserViews to urls of users

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path 
-from user.views import get_user_list
+from user.views import userGetView, userCreateView, userUpdateView, userDeleteView
 
 urlpatterns = [
-    path("", get_user_list)
+    path("users/", userGetView, name="user-get"),
+    path("users/create/", userCreateView, name="user-create"),
+    path("users/<int:user_id>/update/", userUpdateView, name="user-update"),
+    path("users/<int:user_id>/delete/", userDeleteView, name="user-delete"),
+
 ]
