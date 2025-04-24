8ce5449580b99280a52d5ba2ed55e59432300f61
Your Name <you@example.com>
2025-04-24 12:49:08 +0330

    Create ResportCard serializer

---
+++ reportcard/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers 
+from reportcard.models import ReportCard 
+
+class ResportCardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReportCard 
+        fields = [
+            "user",
+            "class_room",
+            "scores",
+            "disciplinary_status",
+            "grade",
+            "created_at"
+        ]
