91e5edf0ccbe37bf9b4456fa5bff4e21a3c646a7
Your Name <you@example.com>
2025-04-27 19:31:20 +0330

    Create url for POST method of reportcard

--- reportcard/urls.py
+++ reportcard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from reportcard.views import reportcardGetView 
+from reportcard.views import reportcardGetView , reportcardPostView
 
 urlpatterns = [
-    path("reportcard/", reportcardGetView, name="Get ReportCards")
+    path("reportcard/", reportcardGetView, name="Get ReportCards"),
+    path("reportcard/create/", reportcardPostView, name="Post ReportCards")
 ]
