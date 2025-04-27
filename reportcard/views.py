2639faf473f588bb601020ae1b7fdd5b326f9838
Your Name <you@example.com>
2025-04-27 19:30:31 +0330

    Create POST method for reportcard API

--- reportcard/views.py
+++ reportcard/views.py
@@ -1,12 +1,26 @@
+from score.models import Score
 from reportcard.models import ReportCard 
 from reportcard.serializers import ReportCardSerializer
 from common.is_authenticated import authenticated_required
+from common.is_admin import admin_required
 from rest_framework.decorators import api_view 
 from rest_framework.response import Response 
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+
+def calculate_average(user):
+    scores = Score.objects.filter(students=user)
+    total_score = sum(score.score_value for score in scores)
+    lesson_count = scores.values("lesson").distinct().count()
+
+    if lesson_count == 0:
+        return 0
+
+    average = total_score / lesson_count
+    return round(average, 2)
+
 
 @swagger_auto_schema(
     method="get",
@@ -46,3 +60,32 @@ def reportcardGetView(request, *args, **kwargs):
     paginated_queryset = paginated.paginate_queryset(queryset, request)
     serializer = ReportCardSerializer(paginated_queryset, many=True)
     return paginated.get_paginated_response(serializer.data)
+
+@swagger_auto_schema(
+    method="post",
+    request_body=ReportCardSerializer,
+    responses={
+        201: ReportCardSerializer,
+        401: "Authenticated required",
+        400: "Invalid data",
+        403: "Forbidden",
+    }
+)
+@api_view(["POST"])
+@authenticated_required
+@admin_required
+def reportcardPostView(request):
+    user = request.user 
+    average = calculate_average(user)
+    serializer = ReportCardSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(grade=average)
+        return Response(
+            {"detail":"Report Card created successfully!", "data":serializer.data},
+            status=status.HTTP_201_CREATED
+        )
+    else:
+        return Response(
+            {"detail":"Invalid data", "errors":serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
