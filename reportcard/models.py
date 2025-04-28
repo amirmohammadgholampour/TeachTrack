f71965e1eb82a0b1152beee5e6567c6498d9bc40
Your Name <you@example.com>
2025-04-28 19:33:39 +0330

    Add  and  to grade field

--- reportcard/models.py
+++ reportcard/models.py
@@ -35,7 +35,7 @@ class ReportCard(models.Model):
         verbose_name="Discplinary status"
     )
 
-    grade = models.DecimalField(max_digits=5, decimal_places=2)
+    grade = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
