6f6cb7ddc3612e9cca0437396d187c89775a8222
Your Name <you@example.com>
2025-04-28 19:35:34 +0330

    changing grade field to method field

--- reportcard/serializers.py
+++ reportcard/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers 
 from reportcard.models import ReportCard 
+from score.models import Score 
 
 class ReportCardSerializer(serializers.ModelSerializer):
+    grade = serializers.SerializerMethodField()
     class Meta:
         model = ReportCard 
         fields = [
@@ -12,3 +14,14 @@ class ReportCardSerializer(serializers.ModelSerializer):
             "grade",
             "created_at"
         ]
+
+    def get_grade(self, obj):
+        scores = obj.scores.all()
+        total_score = sum(score.score_value for score in scores)
+        lesson_count = scores.values("lesson").distinct().count()
+
+        if lesson_count == 0:
+            return 0
+        
+        average = total_score / lesson_count
+        return round(average, 2)
