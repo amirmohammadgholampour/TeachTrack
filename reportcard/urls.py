1017c91d8fc82f94ec2183d3cc23281894e34bf1
Your Name <you@example.com>
2025-04-29 21:57:33 +0330

    Create urls for put method of reportcard view

--- reportcard/urls.py
+++ reportcard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from reportcard.views import reportcardGetView , reportcardPostView
+from reportcard.views import reportcardGetView , reportcardPostView, reportcardPutView
 
 urlpatterns = [
     path("reportcard/", reportcardGetView, name="Get ReportCards"),
-    path("reportcard/create/", reportcardPostView, name="Post ReportCards")
+    path("reportcard/create/", reportcardPostView, name="Post ReportCards"),
+    path("reportcard/<int:reportcard_id>/update", reportcardPutView, name="Put ReportCards"),
 ]
