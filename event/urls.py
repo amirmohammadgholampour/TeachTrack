348bcac2f463cd3b97cc9fb639638a065e1fd4e4
Your Name <you@example.com>
2025-05-02 22:35:30 +0330

    Add GET method to url

--- event/urls.py
+++ event/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path 
 from event.views import eventGetView, eventPostView, eventPutView, eventDeleteView
+from event.registration_views import registerGetView, registerPostView
 
 urlpatterns = [
     path("event/", eventGetView, name="Get Events"),
     path("event/create/", eventPostView, name="Post Event"),
     path("event/<int:event_id>/update/", eventPutView, name="Put Event"),
-    path("event/<int:event_id>/delete/", eventDeleteView, name="Delete Event")
+    path("event/<int:event_id>/delete/", eventDeleteView, name="Delete Event"),
+
+    path("event/register/", registerGetView, name="Get Registration"),
+    path("event/register/create/", registerPostView, name="Post Registration"),
 ]
