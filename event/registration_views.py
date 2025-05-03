f1891f70ea5c13500408e64164fa6966a2a3bbae
Your Name <you@example.com>
2025-05-03 05:54:17 +0330

    Create POST method for registration API

--- event/registration_views.py
+++ event/registration_views.py
@@ -58,22 +58,43 @@ def registerGetView(request, *args, **kwargs):
     serializer = RegistrationSerializer(paginated_queryset, many=True) 
     return paginated.get_paginated_response(serializer.data)
 
-# @swagger_auto_schema(
-#     method="post",
-#     request_body=RegistrationSerializer
-# )
-# @api_view(["POST"])
-# @authenticated_required
-# def registerPostView(request):
-#     serializer = RegistrationSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(
-#             {"detail":"Registered successfully!", "data":serializer.data},
-#             status=status.HTTP_201_CREATED
-#         )
-#     else:
-#         return Response(
-#             {"detail":"Invalid data", "errors":serializer.errors},
-#             status=status.HTTP_400_BAD_REQUEST
-#         )
+@swagger_auto_schema(
+    method="post",
+    request_body=RegistrationSerializer
+)
+@api_view(["POST"])
+@authenticated_required
+def registerPostView(request, *args, **kwargs):
+    event_capacity = request.data.get("event__capacity")
+    if event_capacity == "completed":
+        return Response(
+            {"detail":"Compacity for this event is full"},
+            status=status.HTTP_403_FORBIDDEN
+        )
+    
+    event_status = request.data.get("event__status")
+    if event_status in ["held", "in_progress"]:
+        return Response(
+            {"detail":"This event is not open for registrations"},
+            status=status.HTTP_403_FORBIDDEN
+        )
+    
+    req_user=request.user
+    if Registration.objects.filter(user=req_user).count() >= 3:
+        return Response(
+            {"detail":"You are not allowed to registration"},
+            status=status.HTTP_403_FORBIDDEN
+        )
+    
+    serializer = RegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=req_user)
+        return Response(
+            {"detail":"Registered successfully!", "data":serializer.data},
+            status=status.HTTP_201_CREATED
+        )
+    else:
+        return Response(
+            {"detail":"Invalid data", "errors":serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
