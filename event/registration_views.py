91aca1c0f81bf2c964b4eb3dbbafbf2ce7aa18bd
Your Name <you@example.com>
2025-05-03 18:59:43 +0330

    Create PUT method for registration API View

--- event/registration_views.py
+++ event/registration_views.py
@@ -2,13 +2,14 @@ from event.models import Registration
 from event.serializers import RegistrationSerializer
 from rest_framework.decorators import api_view
 from common.is_authenticated import authenticated_required
+from common.is_admin import admin_required
 from rest_framework.response import Response 
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi 
 from django.db.models import Q 
-from datetime import datetime, date
+from datetime import date
 
 
 @api_view(["GET"])
@@ -64,6 +65,7 @@ def registerGetView(request, *args, **kwargs):
 )
 @api_view(["POST"])
 @authenticated_required
+@admin_required
 def registerPostView(request, *args, **kwargs):
     event_capacity = request.data.get("event__capacity")
     if event_capacity == "completed":
@@ -92,6 +94,57 @@ def registerPostView(request, *args, **kwargs):
         return Response(
             {"detail":"Registered successfully!", "data":serializer.data},
             status=status.HTTP_201_CREATED
+        )
+    else:
+        return Response(
+            {"detail":"Invalid data", "errors":serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+@swagger_auto_schema(
+    method="put",
+    request_body=RegistrationSerializer
+)
+@api_view(["PUT"])
+@authenticated_required
+@admin_required
+def registerPutView(request, *args, **kwargs):
+    event_capacity = request.data.get("event__capacity")
+    if event_capacity == "completed":
+        return Response(
+            {"detail":"Compacity for this event is full"},
+            status=status.HTTP_403_FORBIDDEN
+        )
+    
+    event_status = request.data.get("event__status")
+    if event_status in ["held", "in_progress"]:
+        return Response(
+            {"detail":"This event is not open for registrations"},
+            status=status.HTTP_403_FORBIDDEN
+        )
+    
+    req_user=request.user
+    if Registration.objects.filter(user=req_user).count() >= 3:
+        return Response(
+            {"detail":"You are not allowed to registration"},
+            status=status.HTTP_403_FORBIDDEN
+        )
+
+    register_id = kwargs.get("registration_id")
+    try:
+        registration = Registration.objects.get(id=register_id)
+    except Registration.DoesNotExist:
+        return Response(
+            {"detail":"Registration not found"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    serializer = RegistrationSerializer(registration, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {"detail":"Registration updated successfully!", "data":serializer.data},
+            status=status.HTTP_200_OK
         )
     else:
         return Response(
