b011f909cf221898659c0acd888d44bf2d7a26b1
Your Name <you@example.com>
2025-05-03 05:54:59 +0330

    Create url for POST method of registration View

--- event/urls.py
+++ event/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from event.views import eventGetView, eventPostView, eventPutView, eventDeleteView
-from event.registration_views import registerGetView
+from event.registration_views import registerGetView, registerPostView
 
 urlpatterns = [
     path("event/", eventGetView, name="Get Events"),
@@ -9,5 +9,5 @@ urlpatterns = [
     path("event/<int:event_id>/delete/", eventDeleteView, name="Delete Event"),
 
     path("event/register/", registerGetView, name="Get Registration"),
-    # path("event/register/create/", registerPostView, name="Post Registration"),
+    path("event/register/create/", registerPostView, name="Post Registration"),
 ]
