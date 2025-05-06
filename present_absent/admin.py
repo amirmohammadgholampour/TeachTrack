bdca50233e6535119970006821d332b8bd2b8251
Your Name <you@example.com>
2025-05-06 17:34:58 +0330

    Create PresentAbsent admin panel

--- present_absent/admin.py
+++ present_absent/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
+from django.contrib import admin 
+from present_absent.models import PresentAbsent
 
-# Register your models here.
+@admin.register(PresentAbsent)
+class PresentAbsentAdmin(admin.ModelAdmin):
+    list_display = [
+        "user__username", 
+        "classroom",
+        "status"
+    ]
+    list_editable = ["status"]
+    list_filter = [
+        "status",
+        "classroom__field__name",
+        "classroom__base",
+    ]
+    list_per_page = 10
+    ordering = ["status"]
+    search_fields = [
+        "user__username",
+        "classroom__nmae"
+    ]
