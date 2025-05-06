195895c69b36a674e67e6ff4fc1c65bedd5c46d2
Your Name <you@example.com>
2025-05-06 17:12:28 +0330

    Create PresentAbsent Model

--- present_absent/models.py
+++ present_absent/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.db import models 
+from user.models import User 
+from classroom.models import ClassRoom 
 
-# Create your models here.
+class PresentAbsent(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={"user_type":"student"},
+        verbose_name="Student Users"   
+    )
+
+    classroom = models.ForeignKey(
+        ClassRoom,
+        on_delete=models.CASCADE,
+        verbose_name="Class room"
+    )
+
+    STATUS_CHOICES = [
+        ("absent", "Absent"),
+        ("present", "Present")
+    ]
+    status = models.CharField(
+        max_length=255,
+        choices=STATUS_CHOICES,
+        verbose_name="Status"
+    )
+
+    def __str__(self):
+        return f"student: {self.user.username}({self.status})"
