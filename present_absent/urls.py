38022e039c89fe656111fcf66b5f040cf49ab8b4
Your Name <you@example.com>
2025-05-06 21:36:23 +0330

    Add POST method to urls

--- present_absent/urls.py
+++ present_absent/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from present_absent.views import getAttendingView
+from present_absent.views import getAttendingView, postAttendingView
 
 urlpatterns = [
     path("attending/", getAttendingView),
+    path("attending/create/", postAttendingView),
 ]
