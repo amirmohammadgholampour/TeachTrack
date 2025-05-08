7254af4848ceaa7fe9d41204903a1b1cbbe51741
Your Name <you@example.com>
2025-05-08 21:05:21 +0330

    Create PUT method for Atteding View

--- present_absent/views.py
+++ present_absent/views.py
@@ -132,6 +132,51 @@ def postAttendingView(request):
     
 @api_view(["PUT"])
 def putAttendingView(request, *args, **kwargs):
+    
+    classroom_id = request.data.get("classroom")
+    user_id = request.data.get("user")
+    user = request.user
+    
+    if (not user.user_type in ["admin", "teacher"]) and (user.is_staff != True):
+        return Response(
+            {"detail":"Only teachers and administrators have the right to register student attendance."},
+            status=status.HTTP_403_FORBIDDEN
+        )
+    
+    user = User.objects.filter(id=user_id).first()
+    if not user or user.user_type != "student":
+        return Response(
+            {"detail": "Only students can take attendance."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    if user.is_active == False:
+        return Response(
+            {"detail":"This student is inactive."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    if not ClassRoom.objects.filter(id=classroom_id, students__id=user_id).exists():
+        return Response(
+            {"detail": "Mismatch between selected classroom and user."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    req_date = datetime.strptime(request.data.get("date"), "%Y-%m-%d")
+    main_date = PresentAbsent.objects.filter(user=user_id, date=req_date)
+    if main_date.exists():
+        return Response(
+            {"detail":"This user with this date is already exist."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    present_date = datetime.today()
+    if req_date > present_date:
+        return Response(
+            {"detail":"You cannot record attendance for a future date."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
     attending_id = kwargs.get("attending_id")
     try:
         PresentAbsent.objects.get(id=attending_id)
@@ -139,4 +184,17 @@ def putAttendingView(request, *args, **kwargs):
         return Response(
             {"detail":"Attending not found."},
             status=status.HTTP_404_NOT_FOUND
+        )
+    
+    serializer = PresentAbsentSerializer(data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {"detail":"Attending updated successfully!", "data": serializer.data},
+            status=status.HTTP_200_OK
+        )
+    else:
+        return Response(
+            {"detail":"Invalid data", "errors":serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
         )
