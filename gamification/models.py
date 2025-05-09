2935ddbe6cb7eabeaf0db520bd7605d7a8245a9e
Your Name <you@example.com>
2025-05-09 09:56:26 +0330

    Updated

--- gamification/models.py
+++ gamification/models.py
@@ -8,53 +8,44 @@ class StudentProfile(models.Model):
         limit_choices_to={"user_type":"student"},
         verbose_name="Student"
     )
-
     total_point = models.IntegerField(
         default=0,
         verbose_name="Total points"
     )
-
     level = models.IntegerField(
         default=1,
         verbose_name="Level"
     )
-
     def __str__(self):
         return f'{self.students.username}(total points: {self.total_point}) (Level: {self.level})'
     
-
 class EventType(models.Model):
     code = models.CharField(
         max_length=100,
         unique=True,
         verbose_name="Code of work"
     )
-    
     name = models.CharField(
         max_length=255,
         verbose_name="Name"
     )
-
     description = models.TextField(verbose_name="Description")
     point = models.IntegerField(verbose_name="Point")
 
     def __str__(self):
         return f'{self.name} (+{self.point}) (code: {self.code})'
     
-
 class StudentEvent(models.Model):
     student_profile = models.ForeignKey(
         StudentProfile,
         on_delete=models.CASCADE,
         related_name="StudentEvent",
     )
-
     event_type = models.ForeignKey(
         EventType,
         on_delete=models.CASCADE,
         verbose_name="Event Type"
     )
-
     created_at = models.DateTimeField(auto_now_add=True)
     note = models.CharField(max_length=255)
 
