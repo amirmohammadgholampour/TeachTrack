01afb648fa8a4afc2941af722fe50ed0a01a63c1
Your Name <you@example.com>
2025-05-09 09:16:12 +0330

    Create StudentProfile for post of total point and level automatic

--- gamification/models.py
+++ gamification/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from django.db import models 
+from user.models import User
 
-# Create your models here.
+class StudentProfile(models.Model):
+    students = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        limit_choices_to={"user_type":"student"},
+        verbose_name="Student"
+    )
+
+    total_point = models.IntegerField(
+        default=0,
+        verbose_name="Total points"
+    )
+
+    level = models.IntegerField(
+        default=1,
+        verbose_name="Level"
+    )
+
+    def __str__(self):
+        return f'{self.students.username}(total points: {self.total_point}) (Level: {self.level})'
