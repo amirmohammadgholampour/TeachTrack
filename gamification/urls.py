d179c53880cf6f1c21e0db7f9447b7511fd41652
Your Name <you@example.com>
2025-05-12 06:38:13 +0330

    Create url for getBestStudetInSchool GET method

--- gamification/urls.py
+++ gamification/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from gamification.views import (
     StudentProfileView,
-    
+    getBestStudentInSchool
 )
 urlpatterns = [
     path('gamification/student-profile/', StudentProfileView.as_view(), name='student_profile'),
+    path('gamification/top-students/', getBestStudentInSchool),
 ]
