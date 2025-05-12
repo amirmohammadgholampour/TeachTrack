1edcab1308ffe2dcc18de265f19abd6f2ad1ec50
Your Name <you@example.com>
2025-05-12 08:18:14 +0330

    import signals.py in apps.py on present_absent app

--- present_absent/apps.py
+++ present_absent/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PresentAbsentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'present_absent'
+
+    def ready(self):
+        import present_absent.signals
