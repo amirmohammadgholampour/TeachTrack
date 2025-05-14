468b8a63464a4ce69a1a48e3b46a490a20b0ce29
Your Name <you@example.com>
2025-05-14 09:51:01 +0330

    Create test for student profile POST method

--- gamification/tests/test_studentprofile.py
+++ gamification/tests/test_studentprofile.py
@@ -61,3 +61,84 @@ class TestGetStudentProfileView:
 
         # Assert
         response.status_code == status.HTTP_200_OK
+
+@pytest.mark.django_db
+class TestPostStudentProfileView:
+    def test_if_usertype_is_not_admin_return_403(self):
+        # Arrange 
+        user = User.objects.create_user(
+            username="test_student1",
+            password="string1234",
+            user_type="student",
+            email="student@domain.com",
+            phone_number="09309500547",
+            national_code="0960031122" 
+        )
+        client = APIClient()
+        client.force_authenticate(user=user)
+
+        # Act 
+        response = client.post(
+            "/gamification/student-profile/",
+        {
+            "students": 2,
+            "total_point": 50,
+            "level": 5
+        }
+        )
+
+        # Assert
+        response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_usertype_is_admin_return_201(self):
+        # Arrange 
+        user = User.objects.create_user(
+            username="test_admin1",
+            password="admin1234",
+            user_type="admin",
+            email="admin@gmail.com",
+            phone_number="09309500547",
+            national_code="0960031122" 
+        )
+        client = APIClient()
+        client.force_authenticate(user=user)
+
+        # Act 
+        response = client.post(
+            "/gamification/student-profile/",
+        {
+            "students": 2,
+            "total_point": 50,
+            "level": 5
+        }
+        )
+
+        # Assert
+        response.status_code == status.HTTP_201_CREATED
+    
+    def test_if_user_enter_invalid_data_return_400(self):
+        # Arrange 
+        user = User.objects.create_user(
+            username="test_admin1",
+            password="admin1234",
+            user_type="admin",
+            email="admin@gmail.com",
+            phone_number="09309500547",
+            national_code="0960031122" 
+        )
+        client = APIClient()
+        client.force_authenticate(user=user)
+
+        # Act 
+        response = client.post(
+            "/gamification/student-profile/",
+        {
+            "students": 0,
+            "total_point": 50,
+            "level": 5
+        }
+        )
+
+        # Assert
+        response.status_code == status.HTTP_400_BAD_REQUEST
+        
