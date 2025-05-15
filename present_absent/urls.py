736bacb171d7285348a57f2199383ee811d303b2
Your Name <you@example.com>
2025-05-15 19:30:18 +0330

    Create url for Attendance Approval GET method

--- present_absent/urls.py
+++ present_absent/urls.py
@@ -5,10 +5,15 @@ from present_absent.views import (
     putAttendingView,
     deleteAttendingView
 )
+from present_absent.attendance_approvald_views import (
+    getAttendanceApprovalView
+)
 
 urlpatterns = [
     path("attending/", getAttendingView),
     path("attending/create/", postAttendingView),
     path("attending/<int:attending_id>/update/", putAttendingView),
     path("attending/<int:attending_id>/delete/", deleteAttendingView),
+
+    path("attending/approval/", getAttendanceApprovalView),
 ]
