0b14ab1cb36ef16d562c4b43f74a54f67c81b7eb
Your Name <you@example.com>
2025-05-15 19:04:26 +0330

    Create Attendance Approval and Review serializer

--- present_absent/serializers.py
+++ present_absent/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers 
-from present_absent.models import PresentAbsent 
+from present_absent.models import (
+    PresentAbsent,
+    AttendanceApproval,
+    AttendanceReview
+)
 
 class PresentAbsentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +13,26 @@ class PresentAbsentSerializer(serializers.ModelSerializer):
             "classroom",
             "status",
             "date"
+        ]
+
+class AttendanceApprovalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AttendanceApproval
+        fields = [
+            "teacher",
+            "student",
+            "classroom",
+            "status_requested",
+            "date",
+            "created_at"
+        ]
+
+class AttendanceReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AttendanceReview
+        fields = [
+            "attending_approval",
+            "review_status",
+            "reviewed_by",
+            "reviewed_at"
         ]
