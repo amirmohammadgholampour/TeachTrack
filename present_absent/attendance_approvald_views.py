2672b62e47e5e30e88e2a96c98ad808558b12583
Your Name <you@example.com>
2025-05-16 09:45:34 +0330

    Create POST method for AttendanceApproval APIView

--- present_absent/attendance_approvald_views.py
+++ present_absent/attendance_approvald_views.py
@@ -1,5 +1,7 @@
-from present_absent.models import AttendanceApproval
+from present_absent.models import AttendanceApproval, PresentAbsent 
 from present_absent.serializers import AttendanceApprovalSerializer
+from user.models import User 
+from classroom.models import ClassRoom
 from rest_framework.decorators import api_view 
 from rest_framework import status 
 from rest_framework.response import Response 
@@ -7,6 +9,7 @@ from rest_framework.pagination import PageNumberPagination
 from common.is_authenticated import authenticated_required
 from drf_yasg.utils import swagger_auto_schema 
 from drf_yasg import openapi 
+from datetime import datetime
 
 @swagger_auto_schema(
     method="get",
@@ -55,3 +58,66 @@ def getAttendanceApprovalView(request, *args, **kwargs):
     paginated_queryset = paginated.paginate_queryset(queryset, request)
     serializer = AttendanceApprovalSerializer(paginated_queryset, many=True)
     return paginated.get_paginated_response(serializer.data)
+
+@swagger_auto_schema(
+    method="post",
+    request_body=AttendanceApprovalSerializer,
+)
+@api_view(["POST"])
+@authenticated_required
+def postAttendanceApprovalView(request, *args, **kwargs):
+    req_user = request.user 
+    if (not req_user.user_type in ["admin", "teacher"]) and (req_user.is_staff != True):
+        return Response(
+            {"detail":"Only teachers and administrators have the right to register student attendance."},
+            status=status.HTTP_403_FORBIDDEN
+        )
+    
+    student_id = request.data.get("student")
+    user = User.objects.filter(id=student_id).first()
+    if not user or user.user_type != "student":
+        return Response(
+            {"detail": "Only students can take attendance."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    if user.is_active == False:
+        return Response(
+            {"detail":"This student is inactive."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    classroom_id = request.data.get("classroom")
+    if not ClassRoom.objects.filter(id=classroom_id, students__id=student_id).exists():
+        return Response(
+            {"detail": "Mismatch between selected classroom and user."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    req_date = datetime.strptime(request.data.get("date"), "%Y-%m-%d")
+    main_date = AttendanceApproval.objects.filter(student=student_id, date=req_date)
+    if main_date.exists():
+        return Response(
+            {"detail":"This user with this date is already exist."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    present_date = datetime.today()
+    if req_date > present_date:
+        return Response(
+            {"detail":"You cannot record attendance for a future date."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    serializer = AttendanceApprovalSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {"detail":"Attendance created successfully!", "data":serializer.data},
+            status=status.HTTP_201_CREATED
+        )
+    else:
+        return Response(
+            {"detail":"Invalid data.", "errors":serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        ) 
